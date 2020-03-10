e914aa04a731e1bf182f1d016b2efe13a7991983
github username <github email address>
2020-03-10 17:11:52 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,20 +6,19 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.gender === 'female' ? '♀' : '♂'}
+            PET NAME {this.props.pet.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">PET TYPE {this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: PET AGE {this.props.pet.age}</p>
+            <p>Weight: PET WEIGHT {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          <button className={this.props.pet.isAdopted ? "ui disabled button" : "ui primary button"}  onClick={() => { if(!(this.props.pet.isAdopted)){ this.props.onAdoptPet(this.props.pet.id)}}} >{this.props.pet.isAdopted ? "Already Adopted" : "Adopt pet"}</button>
         </div>
       </div>
     )
